8b1d14a105a3ece9fdb6971f1506a03e86219735
alexey <alexey.zaka@gmail.com>
2025-09-16 00:26:51 +0200

    front with out back

--- scriptv3.py
+++ scriptv3.py
@@ -12,12 +12,9 @@ right_clicking = False
 click_type = "left"  # "left" or "right"
 mouse = Controller()
 listener = None
-
-# --- GUI setup ---
-root = tk.Tk()
-root.title("Autoclicker")
-root.geometry("400x300")
-root.resizable(False, False)
+mouse_recording = False
+mouse_events = []
+mouse_listener = None
 
 # Hotkeys
 hotkey_start = keyboard.Key.f7
@@ -64,6 +61,7 @@ def switch_click_type():
     click_type = "right" if click_type == "left" else "left"
     type_label.config(text=f"Current click: {click_type.capitalize()}")
 
+# --- Mouse recording ---
 def on_move(x, y):
     if mouse_recording:
         mouse_events.append(('move', x, y, time.time()))
@@ -129,11 +127,139 @@ def start_listener():
 # --- Settings window ---
 def settings():
     global click_type, hotkey_start, hotkey_stop, hotkey_switch
+
     temp_hotkey_start = hotkey_start
     temp_hotkey_stop = hotkey_stop
     temp_hotkey_switch = hotkey_switch
     temp_click_type = click_type
 
+    settings_window = tk.Toplevel(root)
+    settings_window.title("Settings")
+    settings_window.geometry("400x400")
+    settings_window.resizable(False, False)
+
+    # Background image
+    settings_background = Image.open("settingsbackground.jpg")
+    settings_background = settings_background.resize((484, 512), Image.Resampling.LANCZOS)
+    settings_photo = ImageTk.PhotoImage(settings_background)
+    settings_label = tk.Label(settings_window, image=settings_photo)
+    settings_label.image = settings_photo  
+    settings_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # Info label
+    info_label = tk.Label(settings_window, text="")
+    info_label.pack(pady=10)
+
+    # Start hotkey
+    start_label = tk.Label(settings_window, text=f"Start: {temp_hotkey_start}")
+    start_label.place(x=23, y=10)
+
+    # Stop hotkey
+    stop_label = tk.Label(settings_window, text=f"Stop: {temp_hotkey_stop}")
+    stop_label.place(x=23, y=80)
+
+
+    # Switch hotkey
+    switch_label = tk.Label(settings_window, text=f"Switch: {temp_hotkey_switch}")
+    switch_label.place(x=23, y=150)
+
+    # Recording labels and buttons
+    startrecord_label = tk.Label(settings_window, text=f"Start Recording: {hotkey_record_start}")
+    startrecord_label.place(x=270, y=10)
+
+    stoprecord_label = tk.Label(settings_window, text=f"Stop Recording: {hotkey_record_stop}")
+    stoprecord_label.place(x=270, y=80)
+
+    playrecord_label = tk.Label(settings_window, text=f"Play Recording: {hotkey_play_recording}")
+    playrecord_label.place(x=270, y=150)
+
+    # Click type selection
+    tk.Label(settings_window, text="Choose click type:").place(x=150, y=10)
+    click_var = tk.StringVar(value=temp_click_type)
+    def set_click_type():
+        nonlocal temp_click_type
+        temp_click_type = click_var.get()
+    tk.Radiobutton(settings_window, text="Left", variable=click_var, value="left", command=set_click_type).place(x=175, y=33)
+    tk.Radiobutton(settings_window, text="Right", variable=click_var, value="right", command=set_click_type).place(x=171, y=56)
+
+    # Mouse recording buttons
+    button_record_start = tk.Button(settings_window, text="Start Recording", command=start_mouse_recording, width=15)
+    button_record_start.place(x=280, y=40)
+
+    button_record_stop = tk.Button(settings_window, text="Stop Recording", command=stop_mouse_recording, width=15)
+    button_record_stop.place(x=280, y=110)
+
+    button_play_recording = tk.Button(settings_window, text="Play Recording", command=play_mouse_recording, width=15)
+    button_play_recording.place(x=280, y=180)
+
+    button_resetsettings = tk.Button(settings_window, text="Reset to Default", command=lambda: reset_settings(start_label, stop_label, switch_label, click_var), width=15)
+    button_resetsettings.place(x=140, y=320)
+
+    button_savesettings = tk.Button(settings_window, text="Save Settings", command=lambda: save_settings(), width=15)
+    button_savesettings.place(x=140, y=360)
+    
+    key_switch_button = tk.Button(settings_window, text="Change Switch Key", command=lambda: listen_key("switch"))
+    key_switch_button.place(x=10, y=180)    
+    
+    Change_stop_button = tk.Button(settings_window, text="Change Stop Key", command=lambda: listen_key("stop"))
+    Change_stop_button.place(x=10, y=110)
+    
+    change_start_button = tk.Button(settings_window, text="Change Start Key", command=lambda: listen_key("start"))
+    change_start_button.place(x=10, y=40)
+    
+    # --- Hover эффект для кнопок в окне настроек ---
+    def add_hover_effect(buttons):
+        def on_enter(e):
+            e.widget['background'] = 'grey'
+            e.widget['foreground'] = 'white'
+
+        def on_leave(e):
+            e.widget['background'] = 'SystemButtonFace'
+            e.widget['foreground'] = 'black'
+
+        for btn in buttons:
+            btn.bind("<Enter>", on_enter)
+            btn.bind("<Leave>", on_leave)
+
+    # Применяем к кнопкам настроек
+    add_hover_effect([
+        button_record_start, 
+        button_record_stop, 
+        button_play_recording, 
+        key_switch_button,
+        Change_stop_button,
+        change_start_button
+    ])
+
+    def add_hover_effect_savesettings(buttons):
+        def on_enter(e):
+            e.widget['background'] = 'green'
+            e.widget['foreground'] = 'white'
+
+        def on_leave(e):
+            e.widget['background'] = 'SystemButtonFace'
+            e.widget['foreground'] = 'black'
+
+        for btn in buttons:
+            btn.bind("<Enter>", on_enter)
+            btn.bind("<Leave>", on_leave)
+    add_hover_effect_savesettings([button_savesettings])
+
+    def add_hover_effect_resetsettings(buttons):
+        def on_enter(e):
+            e.widget['background'] = 'red'
+            e.widget['foreground'] = 'white'
+
+        def on_leave(e):
+            e.widget['background'] = 'SystemButtonFace'
+            e.widget['foreground'] = 'black'
+
+        for btn in buttons:
+            btn.bind("<Enter>", on_enter)
+            btn.bind("<Leave>", on_leave)
+    add_hover_effect_resetsettings([button_resetsettings])
+        
+    # --- Nested functions ---
     def listen_key(action):
         info_label.config(text=f"Press the button for {action}...")
 
@@ -169,78 +295,12 @@ def settings():
         click_type = temp_click_type
         type_label.config(text=f"Current click: {click_type.capitalize()}")
         settings_window.destroy()
-    
-    settings_window = tk.Toplevel(root)
-    settings_window.title("Settings")
-    settings_window.geometry("400x400")
-    settings_window.resizable(False, False)
-    
-    # Background image for settings window
-    settings_background = Image.open("settingsbackground.jpg")
-    settings_background = settings_background.resize((484, 512), Image.Resampling.LANCZOS)
-    settings_photo = ImageTk.PhotoImage(settings_background)
-    settings_label = tk.Label(settings_window, image=settings_photo)
-    settings_label.image = settings_photo  
-    settings_label.place(x=0, y=0, relwidth=1, relheight=1)
-    
 
-    # Start hotkey
-    start_label = tk.Label(settings_window, text=f"Start: {temp_hotkey_start}")
-    start_label.place(x=10, y=10)
-    tk.Button(settings_window, text="Change Start Key", command=lambda: listen_key("start")).place(x=10, y=40)
-
-    # Stop hotkey
-    stop_label = tk.Label(settings_window, text=f"Stop: {temp_hotkey_stop}")
-    stop_label.place(x=10, y=80)
-    tk.Button(settings_window, text="Change Stop Key", command=lambda: listen_key("stop")).place(x=10, y=110)
-
-    # Switch hotkey
-    switch_label = tk.Label(settings_window, text=f"Switch: {temp_hotkey_switch}")
-    switch_label.place(x=10, y=150)
-    tk.Button(settings_window, text="Change Switch Key", command=lambda: listen_key("switch")).place(x=10, y=180)
-    
-    startrecord_label = tk.Label(settings_window, text=f"Start Recording: {hotkey_record_start}")
-    startrecord_label.place(x=270, y=10)
-
-    stoprecord_label = tk.Label(settings_window, text=f"Stop Recording: {hotkey_record_stop}")
-    stoprecord_label.place(x=270, y=80)
-
-    playrecord_label = tk.Label(settings_window, text=f"Play Recording: {hotkey_play_recording}")
-    playrecord_label.place(x=270, y=150)
-
-    # Click type selection
-    tk.Label(settings_window, text="Choose click type:").pack(pady=10)
-    click_var = tk.StringVar(value=temp_click_type)
-    def set_click_type():
-        nonlocal temp_click_type
-        temp_click_type = click_var.get()
-    tk.Radiobutton(settings_window, text="Left", variable=click_var, value="left", command=set_click_type).pack()
-    tk.Radiobutton(settings_window, text="Right", variable=click_var, value="right", command=set_click_type).pack()
-
-    
-    # Info label
-    info_label = tk.Label(settings_window, text="")
-    info_label.pack(pady=10)
-
-    # Mouse recording buttons
-    button_record_start = tk.Button(settings_window, text="Change Start Rec", command=start_mouse_recording, width=15)
-    button_record_start.place(x=280, y=40)
-
-    button_record_stop = tk.Button(settings_window, text="Change Stop Rec", command=stop_mouse_recording, width=15)
-    button_record_stop.place(x=280, y=110)
-
-    button_play_recording = tk.Button(settings_window, text="Change Play Rec", command=play_mouse_recording, width=15)
-    button_play_recording.place(x=280, y=180)
-    
-    button_resetsettings = tk.Button(settings_window, text="Reset to Default", command=lambda: reset_settings(start_label, stop_label, switch_label, click_var), width=15)
-    button_resetsettings.place(x=140, y=320)
-
-    button_savesettings = tk.Button(settings_window, text="Save Settings", command=save_settings, width=15)
-    button_savesettings.place(x=140, y=360)
-
-
-def recording_settings():
-    pass
+# --- GUI setup ---
+root = tk.Tk()
+root.title("Autoclicker")
+root.geometry("400x300")
+root.resizable(False, False)
 
 # Background image
 background_image = Image.open("background.jpg")
@@ -249,16 +309,44 @@ background_label = tk.Label(root, image=background_photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 # Interval input
-tk.Label(root, text="Interval (sec):").pack(pady=5)
+tk.Label(root, text="Interval (sec):").place(x=165, y=40)
 entry_interval = tk.Entry(root)
 entry_interval.insert(0, "0.5")
-entry_interval.pack(pady=5)
+entry_interval.place(x=140, y=70)
+
+# Number of clicks
+tk.Label(root, text="Number of clicks:").place(x=35, y=100)
+entry_clicks = tk.Entry(root)
+entry_clicks.insert(0, "5")
+entry_clicks.place(x=25, y=130)
+
+# Number of recordings
+tk.Label(root, text="Number of recordings:").place(x=245, y=100)
+entry_recordings = tk.Entry(root)
+entry_recordings.insert(0, "5")
+entry_recordings.place(x=245, y=130)
 
 # Current click type
 type_label = tk.Label(root, text=f"Current click: {click_type.capitalize()}")
 type_label.pack(pady=5)
 
-# Button hover animation
+# Buttons
+button_start = tk.Button(root, text="Start Click", command=start_clicking, width=15)
+button_start.place(x=30, y=165)
+
+button_stop = tk.Button(root, text="Stop Click", command=stop_clicking, width=15)
+button_stop.place(x=30, y=205)
+
+button_settings = tk.Button(root, text="Settings", command=settings, width=15)
+button_settings.place(x=140, y=255)
+
+button_record_start = tk.Button(root, text="Start Recording", command=start_mouse_recording, width=15)
+button_record_start.place(x=250, y=165)
+
+button_record_stop = tk.Button(root, text="Stop Recording", command=stop_mouse_recording, width=15)
+button_record_stop.place(x=250, y=205)
+
+# Hover effect for main window buttons
 def colorbuttons():
     def on_enter(e):
         e.widget['background'] = 'grey' 
@@ -268,26 +356,16 @@ def colorbuttons():
         e.widget['background'] = 'SystemButtonFace' 
         e.widget['foreground'] = 'black'    
 
-    # Attach animation to buttons
-    for btn in [button_start, button_stop, button_settings]:
+    for btn in [
+        button_start,
+        button_stop,
+        button_settings,
+        button_record_start,
+        button_record_stop
+    ]:
         btn.bind("<Enter>", on_enter)
         btn.bind("<Leave>", on_leave)
 
-# buttons
-button_start = tk.Button(root, text="Start", command=start_clicking, width=15)
-button_start.place(x=65, y=215)
-
-button_stop = tk.Button(root, text="Stop", command=stop_clicking, width=15)
-button_stop.place(x=215, y=215)
-
-button_settings = tk.Button(root, text="Settings", command=settings, width=15)
-button_settings.place(x=140, y=255)
-
-button_playrecording = tk.Button(root, text="Play Recording", command=play_mouse_recording, width=15)
-button_playrecording.place(x=145, y=130)
-
 colorbuttons()
-
 start_listener()
-
 root.mainloop()
